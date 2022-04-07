281982df95cdce9117213c8b6f687055d8706620
khawarcrc <82960567+khawarcrc@users.noreply.github.com>
2022-04-08 04:59:52 +0500

    style

--- src/components/TrendingTopicsDetail.js
+++ src/components/TrendingTopicsDetail.js
@@ -39,7 +39,7 @@ const TrendingTopicsDetail = () => {
                 <img src={image1}></img>
               </div>
             </div> */}
-              <p>
+              {/* <p>
                 Today tab: We offer a playlist of sorts with favorite and/or
                 featured content which you might enjoy throughout the day. The
                 Wake Up and Today's Meditation are in the top favorites with our
@@ -49,7 +49,27 @@ const TrendingTopicsDetail = () => {
                 you wind down for the night, sleep better and develop better
                 sleep habits. Our Sleepcasts are a member favorite and we have
                 many to choose from.
-              </p>
+              </p> */}
+              <ul>
+                <li>
+                  <strong className="strongOrange">Today</strong> tab: We offer
+                  a playlist of sorts with favorite and/or featured content
+                  which you might enjoy throughout the day. The Wake Up and
+                  Today's Meditation are in the top favorites with our Headspace
+                  community!
+                </li>
+                <li>
+                  <strong className="strongOrange"> Meditate</strong> tab: We
+                  offer a comprehensive guide featuring our meditation sessions
+                  and courses taught by different teachers.{" "}
+                </li>
+                <li>
+                  <strong>Sleep</strong> tab: We offer tons of options to help
+                  you wind down for the night, sleep better and develop better
+                  sleep habits. Our Sleepcasts are a member favorite and we have
+                  many to choose from.
+                </li>
+              </ul>
               {/* <div className="row">
               <div className="col-md-12">
                 <img src={image4}></img>
@@ -59,17 +79,21 @@ const TrendingTopicsDetail = () => {
                 <img src={image4}></img>
               </div>
               <p>
-                {" "}
-                Move tab: We offer many mind-body fitness sessions guided by
-                expert trainers. The Move sessions are designed to help members
-                strengthen both their mind and their body, together. Focus tab:
-                We offer sounds, music and exercises to help you focus for
-                school, studying, work, tasks or simply to help you relax.
-                Search feature: If you know what you're looking for and you'd
-                rather search for content, you can use our Search field to check
-                out out collections. We hope that's helpful! But, if you have
-                any questions on how to get started, send us a note at
-                help@headspace. We're happy to help!
+                <strong className="strongOrange">Move</strong> tab: We offer
+                many mind-body fitness sessions guided by expert trainers. The
+                Move sessions are designed to help members strengthen both their
+                mind and their body, together.{" "}
+              </p>
+              <p>
+                <strong className="strongOrange">Focus</strong> tab: We offer
+                sounds, music and exercises to help you focus for school,
+                studying, work, tasks or simply to help you relax.{" "}
+              </p>
+              <p>
+                <strong className="strongOrange"> Search</strong> feature: If
+                you know what you're looking for and you'd rather search for
+                content, you can use our Search field to check out out
+                collections.{" "}
               </p>
             </section>
           </div>
