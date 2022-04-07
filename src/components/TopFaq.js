e7e9eb50ad0edbedd70472d5b2e26ae6d66d0867
khawarcrc <82960567+khawarcrc@users.noreply.github.com>
2022-04-08 03:34:39 +0500

    css

--- src/components/TopFaq.js
+++ src/components/TopFaq.js
@@ -5,57 +5,62 @@ import { SearchSVG } from "../images/svg/icons";
 import "./topFaq.css";
 const TopFaq = () => {
   return (
-    <div className="container ">
-      <div className="row">
-        <div className="col-md-5 ">
-          <ol class="breadcrumbs">
-            <li title="Help Center">
-              <a href="/">Help Center</a>
-            </li>
-            <li title="Top FAQ">
-              <a href="#">Top FAQ</a>
-            </li>
-          </ol>
-        </div>
-        <div className="col-md-4 formSection " style={{ marginLeft: "250px" }}>
-          <div className="searchIcon">
-            <SearchSVG />
+    <div className="containerDivider">
+      <div className="container">
+        <div className="row subNav justify-content-between">
+          <div className="col-md-5">
+            <ol class="breadcrumbs">
+              <li title="Help Center">
+                <a href="/">Help Center</a>
+              </li>
+              <li title="Top FAQ">
+                <a href="#">Top FAQ</a>
+              </li>
+            </ol>
           </div>
-          <input
-            type="text"
-            placeholder="Search"
-            className="form-control formInput"
-          ></input>
+          <div className="col-md-5 col-lg-4 formSection ">
+            <div className="searchIcon">
+              <SearchSVG />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="form-control formInput"
+            ></input>
+          </div>
         </div>
-      </div>
-      <div className="row d-flex flex-row justify-content-end">
-        <h3 className="mainHeading">Top FAQ</h3>
-        <div className="col-md-5 ">
-          <h4>
-            <Link to="TrendingTopics">Trending Topics</Link>
-          </h4>
-          {trendingTopics.map((topic, index) => {
-            return (
-              <div key={index}>
-                <Link to={topic.URL}>{topic.Name}</Link>
-                <hr />
-              </div>
-            );
-          })}
-          <Link to="/TrendingTopics">See all 8 articles</Link>
+        <div className="headingWrapper">
+          <h3 className="mainHeading">Top FAQ</h3>
         </div>
-        <div className="col-md-5">
-          <h4>
-            <Link to="KhownIssues">Khown Issues</Link>
-          </h4>
-          {knownIssues.map((knownIssue, index) => {
-            return (
-              <div key={index}>
-                <Link to={knownIssue.URL}>{knownIssue.Name}</Link>
-                <hr />
-              </div>
-            );
-          })}
+        <div className="row d-flex flex-row justify-content-end">
+          <div className="col-md-6 col-xl-5">
+            <h4 className="subHeading">
+              <Link to="TrendingTopics">Trending Topics</Link>
+            </h4>
+            {trendingTopics.map((topic, index) => {
+              return (
+                <div className="articleLinks" key={index}>
+                  <Link to={topic.URL}>{topic.Name}</Link>
+                </div>
+              );
+            })}
+            <span className="seeAllArticles">
+              {" "}
+              <Link to="/TrendingTopics">See all 8 articles</Link>
+            </span>
+          </div>
+          <div className="col-md-6 col-xl-5 pt-5 pt-md-0">
+            <h4 className="subHeading">
+              <Link to="KhownIssues">Khown Issues</Link>
+            </h4>
+            {knownIssues.map((knownIssue, index) => {
+              return (
+                <div className="articleLinks" key={index}>
+                  <Link to={knownIssue.URL}>{knownIssue.Name}</Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/components/TrendingTopicsDetail.js
+++ src/components/TrendingTopicsDetail.js
@@ -5,53 +5,74 @@ import image1 from "../images/iamge1.png";
 import image4 from "../images/image4.png";
 const TrendingTopicsDetail = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          {cardsContent.map((card) => {
-            return (
-              <div className="storeCard">
-                <span className="storeAddress">{card}</span>
-              </div>
-            );
-          })}
-        </div>
+    <div className="containerDivider">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-lg-3">
+            <h3 className="sideNavHeading">Articles in this section</h3>
+            {cardsContent.map((card) => {
+              return (
+                <div className="storeCard">
+                  <span className="storeAddress">{card}</span>
+                </div>
+              );
+            })}
+          </div>
 
-        <div className="col-md-8">
-          New to Headspace? How to get started! If you're new to Headspace or if
-          you've recently returned after a break - welcome! If you're a
-          returning member, you might have noticed we have some new features and
-          content which weren't available before. New stuff is always exciting,
-          but we know it just makes it a little bit harder to figure out how to
-          get started. Below is a simple breakdown on how to navigate our
-          content library.
-          <div className="row">
-            <div className="col-md-3">
-              <img src={image1}></img>
-            </div>
+          <div className="col-md-8">
+            <h2 className="heading2">New to Headspace? How to get started!</h2>
+            <section className="contentSection">
+              <p>
+                New to Headspace? How to get started! If you're new to Headspace
+                or if you've recently returned after a break - welcome! If
+                you're a returning member, you might have noticed we have some
+                new features and content which weren't available before. New
+                stuff is always exciting, but we know it just makes it a little
+                bit harder to figure out how to get started. Below is a simple
+                breakdown on how to navigate our content library.
+              </p>
+              <div className="contentImage">
+                <img src={image1}></img>
+              </div>
+              {/* <div className="row">
+              <div className="col-md-12">
+                <img src={image1}></img>
+              </div>
+            </div> */}
+              <p>
+                Today tab: We offer a playlist of sorts with favorite and/or
+                featured content which you might enjoy throughout the day. The
+                Wake Up and Today's Meditation are in the top favorites with our
+                Headspace community! Meditate tab: We offer a comprehensive
+                guide featuring our meditation sessions and courses taught by
+                different teachers. Sleep tab: We offer tons of options to help
+                you wind down for the night, sleep better and develop better
+                sleep habits. Our Sleepcasts are a member favorite and we have
+                many to choose from.
+              </p>
+              {/* <div className="row">
+              <div className="col-md-12">
+                <img src={image4}></img>
+              </div>
+            </div> */}
+              <div className="contentImage">
+                <img src={image4}></img>
+              </div>
+              <p>
+                {" "}
+                Move tab: We offer many mind-body fitness sessions guided by
+                expert trainers. The Move sessions are designed to help members
+                strengthen both their mind and their body, together. Focus tab:
+                We offer sounds, music and exercises to help you focus for
+                school, studying, work, tasks or simply to help you relax.
+                Search feature: If you know what you're looking for and you'd
+                rather search for content, you can use our Search field to check
+                out out collections. We hope that's helpful! But, if you have
+                any questions on how to get started, send us a note at
+                help@headspace. We're happy to help!
+              </p>
+            </section>
           </div>
-          Today tab: We offer a playlist of sorts with favorite and/or featured
-          content which you might enjoy throughout the day. The Wake Up and
-          Today's Meditation are in the top favorites with our Headspace
-          community! Meditate tab: We offer a comprehensive guide featuring our
-          meditation sessions and courses taught by different teachers. Sleep
-          tab: We offer tons of options to help you wind down for the night,
-          sleep better and develop better sleep habits. Our Sleepcasts are a
-          member favorite and we have many to choose from.
-          <div className="row">
-            <div className="col-md-3">
-              <img src={image4}></img>
-            </div>
-          </div>
-          Move tab: We offer many mind-body fitness sessions guided by expert
-          trainers. The Move sessions are designed to help members strengthen
-          both their mind and their body, together. Focus tab: We offer sounds,
-          music and exercises to help you focus for school, studying, work,
-          tasks or simply to help you relax. Search feature: If you know what
-          you're looking for and you'd rather search for content, you can use
-          our Search field to check out out collections. We hope that's helpful!
-          But, if you have any questions on how to get started, send us a note
-          at help@headspace. We're happy to help!
         </div>
       </div>
     </div>
